38d5c06f0a791ec20c37f24c464e319c8af611fa
Kristjan Tamm <kluis2206@gmail.com>
2021-12-09 11:44:51 +0200

    nupp

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -3,35 +3,27 @@
     <br>
     <h1 style="text-align: center">ADMINI TEGEVUSED:</h1> <br>
     <div class="text-center align-center justify-space-around">
-      <v-tooltip bottom>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-              class="mx-2"
-              fab
-              dark
-              color="indigo"
-              v-on:click="goToDoc()"
-              v-bind="attrs"
-              v-on="on"
-          ><v-icon dark>
-            mdi-plus
-          </v-icon>
-          </v-btn>
-        </template>
-        <span>Add a new doctor</span>
-      </v-tooltip> <br><br>
-    <v-btn
-        elevation="2"
-        outlined
-        v-on:click="goToNewDate()"
-    >Add new free date for doctor</v-btn> <br><br>
-    <v-btn
-        elevation="2"
-        outlined
-        v-on:click="goToOverall()"
-    >View all reservatioons</v-btn>
+      <v-btn
+          elevation="2"
+          outlined
+          v-on:click="goToDoc()"
+      >Add a new doctor
+      </v-btn> <br><br>
+      <v-btn
+          elevation="2"
+          outlined
+          v-on:click="goToNewDate()"
+      >Add new free date for doctor
+      </v-btn>
+      <br><br>
+      <v-btn
+          elevation="2"
+          outlined
+          v-on:click="goToOverall()"
+      >View all reservatioons
+      </v-btn>
 
-  </div>
+    </div>
   </div>
 </template>
 
